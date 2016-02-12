ad117e05a8765920be01583eab91bcab961149cf
Phil Custer <custer55@gmail.com>
2016-02-11 20:13:49 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
+from mainWindow import Ui_MainWindow
+from PyQt5 import uic
+import sys
+
+def compileUIC():
+    uic.compileUiDir('./')
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.quitButton.clicked.connect(self.close)
+
+
+if __name__ == '__main__':
+    compileUIC()
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
