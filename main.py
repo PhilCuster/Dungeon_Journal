374c73aa34e8b6778228c200314daa50507c7b5c
PhilCuster <vengevine763>
2016-02-12 11:41:35 -0500

    Clicking the "Create Template" button opens a new window...

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication
 from mainWindow import Ui_MainWindow
+from newTemplate import Ui_newTemplate
 from PyQt5 import uic
 import sys
 
 def compileUIC():
     uic.compileUiDir('./')
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,6 +16,22 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         self.ui.quitButton.clicked.connect(self.close)
+        self.ui.createTemplateButton.clicked.connect(self.createNewTemplate)
+        self.w = None
+
+    def createNewTemplate(self):
+        self.hide()
+        self.w = newTemplate()
+        self.w.show()
+
+
+class newTemplate(QWidget):
+    def __init__(self):
+        super(newTemplate, self).__init__()
+
+        self.ui = Ui_newTemplate()
+        self.ui.setupUi(self)
+
 
 
 if __name__ == '__main__':
---
+++ newTemplate.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file './newTemplate.ui'
+#
+# Created by: PyQt5 UI code generator 5.4.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_newTemplate(object):
+    def setupUi(self, newTemplate):
+        newTemplate.setObjectName("newTemplate")
+        newTemplate.resize(448, 397)
+
+        self.retranslateUi(newTemplate)
+        QtCore.QMetaObject.connectSlotsByName(newTemplate)
+
+    def retranslateUi(self, newTemplate):
+        _translate = QtCore.QCoreApplication.translate
+        newTemplate.setWindowTitle(_translate("newTemplate", "Create new template"))
+
